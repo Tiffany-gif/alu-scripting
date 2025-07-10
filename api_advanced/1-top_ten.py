ad62a4de07118a449813a81cc882d488beba3b74
Tiffany-gif <e.umwari@alustudent.com>
2025-07-10 18:06:49 +0000

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-Script to print hot posts on a given Reddit subreddit.
-"""
-
+"""Script to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
@@ -21,6 +17,6 @@ def top_ten(subreddit):
     if response.status_code == 404:
         print("None")
         return
-results = response.json().get("data")
-    
+
+results = response.json().get("data")    
 [print(c.get("data").get("title")) for c in results.get("children")]
