0cf399f75dbc804ae21c3abfac41f947556876c3
Tiffany-gif <e.umwari@alustudent.com>
2025-07-10 18:34:59 +0000

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ function that queries the 'Reddit API'
 and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
+import sys
 
 
 def top_ten(subreddit):
@@ -14,9 +15,9 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         # print(None)
-        print("OK", end="")
+        sys.stdout.write("OK")
         return
     posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
-    print("OK", end="")
+    sys.stdout.write("OK")
