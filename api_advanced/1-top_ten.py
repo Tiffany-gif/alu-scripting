d672c8d9ec6c9623ea45b81e2638d244944e6a10
Tiffany-gif <e.umwari@alustudent.com>
2025-07-10 18:49:03 +0000

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" Queries the Reddit API """
+"""Module for top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts
-        listed for a given subreddit.
-        If an invalid subreddit is given, print None.
-    """
-    user = {"User-Agent": "Mozilla/5.0"}
-    req = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                       .format(subreddit), headers=user)
-    if req.status_code != requests.codes.OK:
-        print('None')
-        return 0
+    """Function that queries the Reddit API."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {}).get('children', [])
+        for post in data:
+            print(post.get('data', {}).get('title'))
     else:
-        for child in req.json().get('data').get('children'):
-            print(child.get('data').get('title'))
+        print(None)
