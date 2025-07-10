2efa62844e81cd3c254f264598b4983a4522f10e
Tiffany-gif <e.umwari@alustudent.com>
2025-07-10 10:03:47 +0000

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""Prints the titles of the first 10 hot posts listed for a given subreddit"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Print top 10 hot posts for a subreddit or None if invalid"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "python:top.ten:v1.0 (by /u/yourusername)"}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print(None)
-        return
-
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        posts = response.json().get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
     except Exception:
         print(None)
-
