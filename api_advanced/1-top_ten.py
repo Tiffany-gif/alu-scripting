b717196f503780827bbda598bb80c692c9b01a00
Tiffany-gif <e.umwari@alustudent.com>
2025-07-10 19:22:09 +0000

    Task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,14 +5,17 @@ import requests
 
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        data = response.json().get('data', {}.get('children'[])
+        if not data:
+            print(None)
+    else:
+        for post in data:
+            print(post.get('data'), {}).get('title'))
     else:
         print(None)
+print("OK", end="")
